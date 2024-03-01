95c71b9c7f58803a8a416c2462a06d35926cbc76
MirzaSameer301 <sameerijaz301@gamil.com>
2024-03-01 11:10:41 +0500

    delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,6 +11,9 @@ import {
   updateUserStart,
   updateUserFailure,
   updateUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess
 } from "../redux/user/userSlice.js";
 
 export default function Profile() {
@@ -85,6 +88,26 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   };
+
+
+  const handleDeleteUser=async()=>{
+    
+    try {
+      dispatch(deleteUserStart())
+      const res=await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE'
+      })
+      const data=await res.json()
+      if(data.success===false){
+        dispatch(deleteUserFailure(data.message))
+        return
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
+
   return (
     <div className="max-w-lg p-3 mx-auto ">
       <h1 className="font-semibold text-center my-7 text-3xl">Profile</h1>
@@ -147,7 +170,7 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-2">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
 
