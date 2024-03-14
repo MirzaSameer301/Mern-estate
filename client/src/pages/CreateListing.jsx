23bf0c13830b6831aa98be1a050ce7d9f75122ec
MirzaSameer301 <sameerijaz301@gamil.com>
2024-03-14 12:03:46 +0500

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -273,7 +273,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
-                <span className="text-xs">($ / month)</span>
+                {formData.type==='rent' &&(
+                  <span className="text-xs">($ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -290,7 +292,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <span>Discounted Price</span>
-                <span className="text-xs">($ / month)</span>
+                {formData.type==='rent' &&(
+                  <span className="text-xs">($ / month)</span>
+                )}
               </div>
             </div>
 
